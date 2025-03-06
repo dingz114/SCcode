4ba7ad20b97dc8f19ca106dc0d3bf19ac101bcbd
白杨 <156503797+114514mbb@users.noreply.github.com>
2025-03-06 16:19:03 +0800

    第三次作业

---
+++ assignment3/shape_class/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+namespace shape_class
+{ 
+   internal class Program
+    {
+        abstract class Shape
+        {
+            public double x, y;
+            public Shape(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public abstract double getArea();
+            public abstract bool isLegal();
+        }
+        class Rectangle : Shape
+        {
+            public Rectangle(double width, double height) : base(width, height)
+            {
+            }
+            public override double getArea()
+            {
+                return x * y;
+            }
+            public override bool isLegal()
+            {
+                if (x != y)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        class Circle : Shape
+        {
+            double radius;
+            public Circle(double radius) : base(radius, radius)
+            {
+                this.radius = radius;
+            }
+            public override double getArea()
+            {
+                return Math.PI * radius * radius;
+            }
+            public override bool isLegal()
+            {
+                if (radius > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        class Square : Rectangle
+        {
+            double side;
+            public Square(double side) : base(side, side)
+            {
+                this.side = side;
+            }
+            public override double getArea()
+            {
+                return side * side;
+            }
+            public override bool isLegal()
+            {
+                if (side > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        class Triangle : Shape
+        {
+            double c;
+            public Triangle(double a, double b, double c) : base(a, b)
+            {
+                this.c = c;
+            }
+            public override double getArea()
+            {
+                double p = (x + y + c) / 2;
+                return Math.Sqrt(p * (p - x) * (p - y) * (p - c));
+            }
+            public override bool isLegal()
+            {
+                if (x + y > c && x + c > y && y + c > x)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(10, 20);
+            if (r1.isLegal()) Console.WriteLine("长方形合法，面积："+r1.getArea());
+            Circle c1 = new Circle(10);
+            if (c1.isLegal()) Console.WriteLine("圆形合法，面积："+c1.getArea());
+            Square s1 = new Square(10);
+            if (s1.isLegal()) Console.WriteLine("正方形合法，面积："+s1.getArea());
+            Triangle t1 = new Triangle(20,20,90);
+            if (t1.isLegal()) Console.WriteLine("三角形合法，面积："+t1.getArea());
+        }
+    }
+}
---
+++ assignment3/shape_class2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace shape_class2
+{
+    public abstract class Shape
+    {
+        public abstract double Area();
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double Area() 
+        { 
+            return Math.PI * radius * radius; 
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public override double Area()
+        {
+            return width* height;
+        }
+    }
+
+    public class ShapeFactory
+    {
+        public static Shape CreateShape(string type, params double[] dim)
+        {
+            switch (type.ToLower())
+            {
+                case "circle":
+                    return new Circle(dim[0]);
+                case "rectangle":
+                    return new Rectangle(dim[0], dim[1]);
+                default:
+                    throw new ArgumentException("Invalid shape type");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Shape> shapes = new List<Shape>();
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                string shapeType = random.Next(2) == 0 ? "Circle" : "Rectangle";
+                if (shapeType == "Circle")
+                {
+                    shapes.Add(ShapeFactory.CreateShape(shapeType, random.NextDouble() * 10));
+                }
+                else
+                {
+                    shapes.Add(ShapeFactory.CreateShape(shapeType, random.NextDouble() * 10, random.NextDouble() * 10));
+                }
+            }
+            double totalArea = 0;
+            foreach (var shape in shapes)
+            {
+                totalArea += shape.Area();
+            }
+
+            Console.WriteLine($"Total area of all shapes: {totalArea}");
+        }
+    }
+}
