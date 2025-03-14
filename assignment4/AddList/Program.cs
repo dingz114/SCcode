c605855876de3ae5f7ef9c2291406d1a866825d3
白杨 <156503797+114514mbb@users.noreply.github.com>
2025-03-14 18:06:51 +0800

    第四次作业

---
+++ assignment4/AddList/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            for (Node<int> node = intlist.Head;
+                  node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+
+            intlist.ForEach(data => Console.WriteLine(data));  //调用ForEach打印链表元素
+
+            int max = int.MinValue;     //求最大值
+            intlist.ForEach(data =>
+            {
+                if (data > max)
+                    max = data;
+            });
+            Console.WriteLine($"最大值: {max}");
+
+            int min = int.MaxValue;     //求最小值
+            intlist.ForEach(data =>
+            {
+                if (data < min)
+                    min = data;
+            });
+            Console.WriteLine($"最小值: {min}");
+
+            int sum = 0;                //求和
+            intlist.ForEach(data => sum += data);
+            Console.WriteLine($"和: {sum}");
+
+
+            // 字符串型List
+            //GenericList<string> strList = new GenericList<string>();
+            //for (int x = 0; x < 10; x++)
+            //{
+            //    strList.Add("str" + x);
+            //}
+            //for (Node<string> node = strList.Head;
+            //        node != null; node = node.Next)
+            //{
+            //    Console.WriteLine(node.Data);
+            //}
+
+           
+        }
+
+    }
+}
---
+++ assignment4/Alarm_clock/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Alarm_clock
+{
+    public class AlarmClock
+    {
+        public event Action Tick;
+        public event Action Alarm;
+
+        public void Start(int time)
+        {
+            for (int i = 0; i < time; i++)
+            {
+                System.Threading.Thread.Sleep(1000); // 每秒触发一次
+                Tick?.Invoke();
+            }
+            Alarm?.Invoke();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 6;
+            AlarmClock clock = new AlarmClock();
+            clock.Tick += () => Console.WriteLine("Tick!");
+            clock.Alarm += () => Console.WriteLine("Alarm!");
+            clock.Start(time);
+        }
+    }
+}
